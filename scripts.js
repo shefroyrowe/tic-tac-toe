4b7915ef0d55dde9524afb8a76ed1c8ce0086eba
shefroyrowe <shefroy.rowe@gmail.com>
2023-08-24 21:27:46 -0500

    html refactored > function to display user moves and push to gameBoard arr added

--- scripts.js
+++ scripts.js
@@ -1,2 +1,28 @@
 //gameboard state tracker
-const gameBoard = [];
+const gameBoard = ['','','','','','','','',''];
+
+let userTurn = true;
+let userMark = 'X';
+
+function displayControl(){
+    let cells = document.querySelectorAll('.cell');
+    cells.forEach(cell=>{
+        cell.addEventListener('click', (e)=>{
+            let { target } = e;
+
+            for(let i = 0; i < gameBoard.length; i++){
+                if(userTurn === true && target.textContent === ''){
+                    cell.textContent = userMark;
+                    console.log(target.id);//REMOVE ME!!
+
+                    if(gameBoard[target.id] === ''){
+                        gameBoard[target.id] = userMark;
+                        console.log(gameBoard);//REMOVE ME!!
+                    }
+                }
+            }
+        });
+    });
+}
+
+displayControl();

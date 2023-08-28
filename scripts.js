c362d0f9419aee6a07b0f9b2afd6ec11069f13d2
shefroyrowe <shefroy.rowe@gmail.com>
2023-08-28 16:51:04 -0500

    logic to log user move to gameboard array added

---
+++ scripts.js
@@ -0,0 +1,32 @@
+const gameBoard = ['','','','','','','','',''];
+
+
+function playerMove() {
+    let playerTurn = 'player1';
+    const player1 = 'X';
+    const player2 = 'O';
+
+    let gameCells = document.querySelectorAll('.cell');
+    gameCells.forEach(function (cell) {
+        cell.addEventListener('click', (e) => {
+            let { target } = e;
+
+            if (gameBoard[target.id] === '' && playerTurn === 'player1'){
+                playerTurn = 'player2';
+                gameBoard[target.id] = player1;
+                console.log(gameBoard);
+            }
+    
+            if (gameBoard[target.id] === '' && playerTurn === 'player2'){
+        playerTurn = 'player1';
+        gameBoard[target.id] = player2;
+        console.log(gameBoard);
+    }
+    });
+});
+    };
+playerMove();
+
+function displayControl() {
+
+};

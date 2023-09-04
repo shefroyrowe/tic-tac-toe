38d9f90fcb047cc3d7733c602ccd49f9738b4da7
shefroyrowe <shefroy.rowe@gmail.com>
2023-09-03 20:41:45 -0500

    status messaging added, progress made in win algorithm

--- scripts.js
+++ scripts.js
@@ -1,49 +1,51 @@
 
-const gameBoard = ['', '', '', '', '', '', '', '', ''];
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
+let gameActive = true;
+let playerTurn = 'player1';
+let currentPlayer = () => {
+    if (playerTurn === 'player1') {
+        return player1;
+    } else if (playerTurn === 'player2') {
+        return player2;
+    }
+
+    return playerTurn;
+};
+const player1 = 'X', player2 = 'O';
+
+
 
 
 function playerMove() {
-
-    let playerTurn = 'player1';
-    let player1Moves = [], player2Moves = [];
-    const player1 = 'X', player2 = 'O';
-
-
 
     let gameCells = document.querySelectorAll('.cell');
     gameCells.forEach(function (cell) {
         cell.addEventListener('click', (e) => {
             let { target } = e;
 
-            if (gameBoard[target.id] === '' && playerTurn === 'player1') {
 
+            if (gameBoard[target.id] === '' && playerTurn === 'player1') {
                 playerTurn = 'player2';
                 gameBoard[target.id] = player1;
-                player1Moves.push(target.id);
                 console.log(gameBoard);
-                console.log(player1Moves);
             }
 
             if (gameBoard[target.id] === '' && playerTurn === 'player2') {
-
                 playerTurn = 'player1';
                 gameBoard[target.id] = player2;
-                player2Moves.push(target.id);
                 console.log(gameBoard);
-                console.log(player2Moves);
             }
-
-
-
         });
+
     });
-    return { gameCells };
+    return { gameCells, playerTurn, player1, currentPlayer };
 }
 playerMove();
 
 
 function displayControl() {
 
+    let statusDisplay = document.querySelector('.game-status');
     let { gameCells } = playerMove();
 
     gameCells.forEach(function (cell) {
@@ -55,21 +57,56 @@ function displayControl() {
             }
         });
     });
+
+    statusDisplay.textContent = `Player ${currentPlayer()}'s turn`;
+    return { statusDisplay };
 }
 displayControl();
 
 
 function findWinner() {
+    let { gameCells } = playerMove();
     const winOptions = [
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
-        [0, 4, 8],
-        [2, 4, 6],
         [0, 3, 6],
-        [2, 5, 8]
-    ]
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
+    let board = gameBoard;
+    gameCells.forEach(function (cell) {
+        cell.addEventListener('click', (e) => {
+            let { target } = e;
+
+            for(let i = 0; i <= 7; i++){
+                let winOps = winOptions[i];
+                console.log(winOpsA[0]);
+            }
+            if (target.textContent === player1) {
+                console.log(`player ${player1} made a move @ ${target.id}`);
+        
+            }
+        });
+    });
+   
+    
+}
+findWinner();
+
+function playGame() {
+    let { statusDisplay } = displayControl();
+    let { gameCells } = playerMove();
+
+    gameActive = true;
+    gameBoard = ['', '', '', '', '', '', '', '', ''];
+    playerTurn = 'player1';
+    statusDisplay.textContent = `Player ${currentPlayer()}'s turn`;
+    gameCells.forEach(cell => cell.textContent = '');
 }
 
-findWinner();
+
+

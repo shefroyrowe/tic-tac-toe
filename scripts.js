62b0f1fca8785a3b2e34c25e5dce4d995d340cae
shefroyrowe <shefroy.rowe@gmail.com>
2023-09-05 22:33:39 -0500

    game operations logic complete > curves added to grid cells

--- scripts.js
+++ scripts.js
@@ -1,53 +1,36 @@
 
 let gameBoard = ['', '', '', '', '', '', '', '', ''];
 let gameOn = true;
-
 const player1 = 'X', player2 = 'O';
-let playerTurn = 'player1', player1Points = 0, player2Points = 0;
-
-const currentPlayer = () => {
-    let { statusDisplay } = displayControl();
-
-    if (playerTurn === 'player1') {
-        statusDisplay.textContent = `Player X's turn`;
-        return player1;
-    }
-
-    if (playerTurn === 'player2') {
-        statusDisplay.textContent = `Player O's turn`;
-        return player2;
-    }
-
-    return playerTurn;
-};
-
-
-
-
+let playerTurn = player1, player1Points = 0, player2Points = 0;
 
 function playerMove() {
-
     let gameCells = document.querySelectorAll('.cell');
     gameCells.forEach(function (cell) {
         cell.addEventListener('click', (e) => {
             let { target } = e;
 
-
-            if (gameBoard[target.id] === '' && playerTurn === 'player1') {
+            if (gameBoard[target.id] === '' &&
+                playerTurn === player1 && gameOn) {
                 gameBoard[target.id] = player1;
+                target.textContent = player1;
                 findWinner();
-                playerTurn = 'player2';
+                playerTurn = player2;
+                console.log(gameBoard);
             }
 
-            if (gameBoard[target.id] === '' && playerTurn === 'player2') {
+            if (gameBoard[target.id] === '' &&
+                playerTurn === player2 && gameOn) {
                 gameBoard[target.id] = player2;
+                target.textContent = player2;
                 findWinner();
-                playerTurn = 'player1';
+                playerTurn = player1;
+                console.log(gameBoard);
             }
         });
 
     });
-    return { gameCells, playerTurn, player1, player2 };
+    return { gameCells, player1, player2 };
 }
 playerMove();
 
@@ -56,21 +39,15 @@ function displayControl() {
     let player1Score = document.getElementById('p1-points');
     let player2Score = document.getElementById('p2-points');
     let statusDisplay = document.querySelector('.game-status');
-    let { gameCells } = playerMove();
-
-    gameCells.forEach(function (cell) {
-        cell.addEventListener('click', (e) => {
-            let { target } = e;
-
-            if (gameBoard[target.id] !== '') {
-                target.textContent = gameBoard[target.id];
-            }
-        });
-    });
+   
+    if(playerTurn === player1){
+        statusDisplay.textContent = `Player ${player2}'s turn`;
+    }else if(playerTurn === player2){
+        statusDisplay.textContent = `Player ${player1}'s turn`;
+    }
 
     return { statusDisplay, player1Score, player2Score };
 }
-displayControl();
 
 
 function findWinner() {
@@ -78,65 +55,68 @@ function findWinner() {
     let { player2Score } = displayControl();
     let { statusDisplay } = displayControl();
 
-    if (gameBoard[0].includes(currentPlayer()) &&
-        gameBoard[1].includes(currentPlayer()) &&
-        gameBoard[2].includes(currentPlayer()) ||
-        gameBoard[3].includes(currentPlayer()) &&
-        gameBoard[4].includes(currentPlayer()) &&
-        gameBoard[5].includes(currentPlayer()) ||
-        gameBoard[6].includes(currentPlayer()) &&
-        gameBoard[7].includes(currentPlayer()) &&
-        gameBoard[8].includes(currentPlayer()) ||
-        gameBoard[0].includes(currentPlayer()) &&
-        gameBoard[3].includes(currentPlayer()) &&
-        gameBoard[6].includes(currentPlayer()) ||
-        gameBoard[1].includes(currentPlayer()) &&
-        gameBoard[4].includes(currentPlayer()) &&
-        gameBoard[7].includes(currentPlayer()) ||
-        gameBoard[2].includes(currentPlayer()) &&
-        gameBoard[5].includes(currentPlayer()) &&
-        gameBoard[8].includes(currentPlayer()) ||
-        gameBoard[0].includes(currentPlayer()) &&
-        gameBoard[4].includes(currentPlayer()) &&
-        gameBoard[8].includes(currentPlayer()) ||
-        gameBoard[2].includes(currentPlayer()) &&
-        gameBoard[4].includes(currentPlayer()) &&
-        gameBoard[6].includes(currentPlayer())) {
+    if (gameBoard[0].includes(playerTurn) &&
+        gameBoard[1].includes(playerTurn) &&
+        gameBoard[2].includes(playerTurn) ||
+        gameBoard[3].includes(playerTurn) &&
+        gameBoard[4].includes(playerTurn) &&
+        gameBoard[5].includes(playerTurn) ||
+        gameBoard[6].includes(playerTurn) &&
+        gameBoard[7].includes(playerTurn) &&
+        gameBoard[8].includes(playerTurn) ||
+        gameBoard[0].includes(playerTurn) &&
+        gameBoard[3].includes(playerTurn) &&
+        gameBoard[6].includes(playerTurn) ||
+        gameBoard[1].includes(playerTurn) &&
+        gameBoard[4].includes(playerTurn) &&
+        gameBoard[7].includes(playerTurn) ||
+        gameBoard[2].includes(playerTurn) &&
+        gameBoard[5].includes(playerTurn) &&
+        gameBoard[8].includes(playerTurn) ||
+        gameBoard[0].includes(playerTurn) &&
+        gameBoard[4].includes(playerTurn) &&
+        gameBoard[8].includes(playerTurn) ||
+        gameBoard[2].includes(playerTurn) &&
+        gameBoard[4].includes(playerTurn) &&
+        gameBoard[6].includes(playerTurn)) {
 
-            if(currentPlayer() === player1 && gameOn === true){
-                player1Points++;
+        if (playerTurn === player1 && gameOn) {
+            statusDisplay.textContent = `Player ${player1} wins!`;
+            player1Points++;
+            player1Score.textContent = player1Points;
+            gameOn = false;
 
-                player1Score.textContent = player1Points;
-                statusDisplay.textContent = `Player ${player1} wins!`;
-                gameOn = false;
-            }else if(currentPlayer() === player2 && gameOn === true){
-                player2Points++;
-        
-                player2Score.textContent = player2Points;
-                statusDisplay.textContent = `Player ${player2} wins!`;
-                gameOn = false;
-            }
+
+            //if (player1Points === 3) {
+                //statusDisplay.textContent = `Player ${player1} wins!`;
+           // }
+        } else if (playerTurn === player2 && gameOn) {
+            statusDisplay.textContent = `Player ${player2} wins!`;
+            player2Points++;
+            player2Score.textContent = player2Points;
+            gameOn = false;
+
+            //if (player2Points === 3) {
+                //statusDisplay.textContent = `Player ${player2} wins!`;
+            //}
+        }
     }
-    if (gameBoard.includes('') === false && gameOn === true) {
+
+    if (gameBoard.includes('') === false && gameOn) {
         statusDisplay.textContent = `Game draw!`;
         gameOn = false;
     }
-  
-
 }
 
-
-
-
-function playGame() {
+function clearGame() {
     let { statusDisplay } = displayControl();
     let { gameCells } = playerMove();
 
-    gameOn = true;
-    gameBoard = ['', '', '', '', '', '', '', '', ''];
-    playerTurn = 'player1';
-    statusDisplay.textContent = `Player ${currentPlayer()}'s turn`;
-    gameCells.forEach(cell => cell.textContent = '');
+        gameOn = true;
+        gameBoard = ['', '', '', '', '', '', '', '', ''];
+        playerTurn = player1;
+        statusDisplay.textContent = `Player ${playerTurn}'s turn`;
+        gameCells.forEach(cell => cell.textContent = '');
 }
 
 

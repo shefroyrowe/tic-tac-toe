06d9e7d06b20613ed7c254cbe812da0a4d975a0d
shefroyrowe <shefroy.rowe@gmail.com>
2023-09-05 23:28:44 -0500

    icon added to tab > html and css updated > js commented

--- scripts.js
+++ scripts.js
@@ -1,31 +1,38 @@
-
+//store user moves and track index on click
 let gameBoard = ['', '', '', '', '', '', '', '', ''];
+
+//tells game when to break operations
 let gameOn = true;
+
+//helper variables to track each player through the game
 const player1 = 'X', player2 = 'O';
 let playerTurn = player1, player1Points = 0, player2Points = 0;
 
+//function to decide who moved and append symbol to correct 
+//gameboard index and gameboard cell display
 function playerMove() {
     let gameCells = document.querySelectorAll('.cell');
     gameCells.forEach(function (cell) {
         cell.addEventListener('click', (e) => {
             let { target } = e;
-
+            
+            //if gameboard index is empty assign user symbol
             if (gameBoard[target.id] === '' &&
                 playerTurn === player1 && gameOn) {
                 gameBoard[target.id] = player1;
                 target.textContent = player1;
-                findWinner();
+
+                findWinner();//search for a win after every move
                 playerTurn = player2;
-                console.log(gameBoard);
             }
 
             if (gameBoard[target.id] === '' &&
                 playerTurn === player2 && gameOn) {
                 gameBoard[target.id] = player2;
                 target.textContent = player2;
-                findWinner();
+
+                findWinner();//search for a win after every move
                 playerTurn = player1;
-                console.log(gameBoard);
             }
         });
 
@@ -34,12 +41,13 @@ function playerMove() {
 }
 playerMove();
 
-
+//function to store elemets needed for announcement displays
 function displayControl() {
     let player1Score = document.getElementById('p1-points');
     let player2Score = document.getElementById('p2-points');
     let statusDisplay = document.querySelector('.game-status');
    
+    //tell user who's move it is next
     if(playerTurn === player1){
         statusDisplay.textContent = `Player ${player2}'s turn`;
     }else if(playerTurn === player2){
@@ -49,12 +57,13 @@ function displayControl() {
     return { statusDisplay, player1Score, player2Score };
 }
 
-
+//function to locate wins and draws
 function findWinner() {
     let { player1Score } = displayControl();
     let { player2Score } = displayControl();
     let { statusDisplay } = displayControl();
 
+    //win conditions block
     if (gameBoard[0].includes(playerTurn) &&
         gameBoard[1].includes(playerTurn) &&
         gameBoard[2].includes(playerTurn) ||
@@ -85,20 +94,22 @@ function findWinner() {
             player1Points++;
             player1Score.textContent = player1Points;
             gameOn = false;
-
-
-            //if (player1Points === 3) {
-                //statusDisplay.textContent = `Player ${player1} wins!`;
-           // }
-        } else if (playerTurn === player2 && gameOn) {
+        } 
+        if (player1Points === 3) {
+            statusDisplay.textContent = `Player ${player1} TAKES THE GAME!!`;
+            gameOn = false;
+        }
+        
+        else if (playerTurn === player2 && gameOn) {
             statusDisplay.textContent = `Player ${player2} wins!`;
             player2Points++;
             player2Score.textContent = player2Points;
             gameOn = false;
+        }
 
-            //if (player2Points === 3) {
-                //statusDisplay.textContent = `Player ${player2} wins!`;
-            //}
+        if (player2Points === 3) {
+            statusDisplay.textContent = `Player ${player2} TAKES THE GAME!!`;
+            gameOn = false;
         }
     }
 
@@ -108,7 +119,10 @@ function findWinner() {
     }
 }
 
+//reset and clear game on click of 'clear' button
 function clearGame() {
+    let { player1Score } = displayControl();
+    let { player2Score } = displayControl();
     let { statusDisplay } = displayControl();
     let { gameCells } = playerMove();
 
@@ -117,6 +131,12 @@ function clearGame() {
         playerTurn = player1;
         statusDisplay.textContent = `Player ${playerTurn}'s turn`;
         gameCells.forEach(cell => cell.textContent = '');
+
+        //if any player reaches three wins, reset scores
+        if(player1Points === 3 || player2Points === 3){
+            player1Score.textContent = '';
+            player1Score.textContent = '';
+        }
 }
 
 

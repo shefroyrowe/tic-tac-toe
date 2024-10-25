de0d6d6a649170007ebfc158ad3e6ac9d4298745
_vision <shefroy.rowe@gmail.com>
2024-10-25 09:31:29 -0400

    add logic to find winner and throw a 'draw/tie' respectively

--- script.js
+++ script.js
@@ -25,37 +25,59 @@ const Gameboard = (() => {
 const PlayGame = (() => {
     const { gameboard, winningMoves } = Gameboard;
     const gameBoxes = document.querySelectorAll('.game-box');
+    let mark_X = [];
+    let mark_O = [];
     let player1Score = 0;
     let player2Score = 0;
     let playerTurn = 'player1';
+    let roundWon = false;
 
     //listen for click on game board to asign player mark
     gameBoxes.forEach(box => box.addEventListener("click", () => {
         if (box.textContent === '' && playerTurn === 'player1') {
+
             box.textContent = 'X';
-            gameboard.push('x');
+            gameboard[Number(box.id)] = 'X';
+            mark_X.push(Number(box.id));
             console.log(gameboard);
+            console.log('x @', mark_X)
+
             playerTurn = 'player2';
-            display.textContent = `${player2.value}'s move`;
+            display.textContent = `${player2.value || 'Player2'}'s move`;
         }
         if (box.textContent === '' && playerTurn === 'player2') {
             box.textContent = 'O';
-            gameboard.push('O');
+            gameboard[Number(box.id)] = 'O';
+            mark_O.push(Number(box.id));
             console.log(gameboard);
+            console.log('o @', mark_O)
+
             playerTurn = 'player1'
-            display.textContent = `${player1.value}'s move`;
+            display.textContent = `${player1.value || 'Player1'}'s move`;
         }
+
+        //find winner/ game draw logic
+        winningMoves.forEach(subArr => {
+                if (subArr.every(value => mark_X.includes(value))) {
+                    console.log('X winns!');
+                }
+                if (subArr.every(value => mark_O.includes(value))) {                   
+                    console.log('O winns!');
+                    
+                }
+                //check if all player moves add up to all 9 gameboard indexes to call a draw
+                if ((mark_X.length + mark_O.length) === 9) {
+                        console.log('It\'s a draw!');
+                }
+        });
     }));
 
-    //find winner
-})();//end playgame()
+})();//end playgame() iife
 
 
 
 /*
     on start game button click clear input fields and show gamegrid/board 
 
-    Clean up the interface to allow players to put in their names, 
-    include a button to start/restart the game and add a display 
-    element that shows the results upon game end!
+    include a button to start/restart the game 
 */
